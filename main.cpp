4b083d30227f324b5794ac880369f69e00ef7fed
Karan <karan.shgl96@gmail.com>
2018-01-29 19:18:08 +0530

    Added resize

--- main.cpp
+++ main.cpp
@@ -1,11 +1,58 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include<string>
+#include <cmath>
 
 using namespace cv;
 using namespace std;
 
 class AffineTransformation{
 public:
+
+  Mat resize(Mat &I, double scale){
+
+    int channels = I.channels();
+    int nRows = I.rows;
+    int nCols = I.cols;
+
+    int newRow = floor(scale*nRows);
+    int newCol = floor(scale*nCols);
+
+    Mat output(newRow, newCol, CV_8UC3);
+
+    double sr = nRows*1.0/newRow;
+    double sc = nCols*1.0/newCol;
+    cout<<sr<<" "<<sc;
+    Vec3b *p,*q;
+
+    for(int i=0;i<newRow;i++){
+      p = I.ptr<Vec3b>(i);
+      q = output.ptr<Vec3b>(i);
+      for(int j=0;j<newCol;j++){
+        int r = floor(i*sr);
+        int c = floor(j*sc);
+
+        double dr = i*sr - r;
+        double dc = j*sc - c;
+
+        for(int k=0;k<channels;k++){
+          q[j][k] = bilinear(I,r,c,dr,dc,k);
+        }
+
+      }
+    }
+    return output;
+
+  }
+
+  int bilinear(Mat &I, int r, int c, double dr, double dc, int k){
+    double val = I.at<Vec3b>(r,c).val[k]*(1-dr)*(1-dc);
+    val += I.at<Vec3b>(r+1,c).val[k]*(dr)*(1-dc);
+    val += I.at<Vec3b>(r,c+1).val[k]*(1-dr)*(dc);
+    val += I.at<Vec3b>(r+1,c+1).val[k]*(dr)*dc;
+    int intensity = round(val);
+    return intensity;
+  }
 
 };
 
@@ -44,8 +91,8 @@ int main( int argc, char** argv ) {
       cout <<  "Could not open or find the image" << endl ;
       return -1;
     }
-  Levels l;
-  image = l.negative(image);
+  AffineTransformation a;
+  image = a.resize(image, 1.5);
   namedWindow( "Display window", WINDOW_AUTOSIZE );
   imshow( "Display window", image );
   
