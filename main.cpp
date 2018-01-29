4a575d5dd632418b51e321de89f153e1e6e5bbaf
Karan <karan.shgl96@gmail.com>
2018-01-29 23:10:32 +0530

    Added Rotation

--- main.cpp
+++ main.cpp
@@ -3,13 +3,49 @@
 #include<string>
 #include <cmath>
 
+#define PI 3.14159265358979323846
+
 using namespace cv;
 using namespace std;
 
+enum Interpolation { nearest, bilinear };
+
 class AffineTransformation{
+
+  int bilinearInterpolation(Mat &I, int r, int c, double dr, double dc, int k){
+    
+    
+
+    double val = I.at<Vec3b>(r,c).val[k]*(1-dr)*(1-dc);
+    val += I.at<Vec3b>(r+1,c).val[k]*(dr)*(1-dc);
+    val += I.at<Vec3b>(r,c+1).val[k]*(1-dr)*(dc);
+    val += I.at<Vec3b>(r+1,c+1).val[k]*(dr)*dc;
+
+    int intensity = round(val);
+
+    return intensity;
+  }
+
+  int nearestNeighbour(Mat &I, int x, int y, int k){
+
+    return I.at<Vec3b>(x,y).val[k];
+  }
+
+  double degree_to_radian(double degree){
+    return degree*PI/180;
+  }
+
+  double radian_to_degree(double radian){
+    return radian*180/PI;
+  }
+
+  bool inRange(int x, int be, int en){
+    return (be <= x && x < en);
+  }
+
 public:
 
-  Mat resize(Mat &I, double scale){
+  Mat resize(Mat &I, double scale, Interpolation ip){
 
     int channels = I.channels();
     int nRows = I.rows;
@@ -20,23 +56,36 @@ public:
 
     Mat output(newRow, newCol, CV_8UC3);
 
-    double sr = nRows*1.0/newRow;
-    double sc = nCols*1.0/newCol;
-    cout<<sr<<" "<<sc;
+    double sr = (nRows-1)*1.0/(newRow-1);
+    double sc = (nCols-1)*1.0/(newCol-1);
+
     Vec3b *p,*q;
 
     for(int i=0;i<newRow;i++){
       p = I.ptr<Vec3b>(i);
       q = output.ptr<Vec3b>(i);
       for(int j=0;j<newCol;j++){
-        int r = floor(i*sr);
-        int c = floor(j*sc);
-
-        double dr = i*sr - r;
-        double dc = j*sc - c;
+        
 
         for(int k=0;k<channels;k++){
-          q[j][k] = bilinear(I,r,c,dr,dc,k);
+          if(ip == nearest){
+
+            int x = round(i*sr);
+            int y = round(j*sc);
+
+            if(inRange(x,0,nRows) && inRange(y, 0, nCols)) q[j][k] = nearestNeighbour(I, x, y ,k);
+          } 
+
+          else if(ip == bilinear){
+
+            int r = floor(i*sr);
+            int c = floor(j*sc);
+
+            double dr = i*sr - r;
+            double dc = j*sc - c;
+
+            if(inRange(r,0,nRows) && inRange(c, 0, nCols)) q[j][k] = bilinearInterpolation(I, r, c, dr, dc, k);
+          } 
         }
 
       }
@@ -45,14 +94,113 @@ public:
 
   }
 
-  int bilinear(Mat &I, int r, int c, double dr, double dc, int k){
-    double val = I.at<Vec3b>(r,c).val[k]*(1-dr)*(1-dc);
-    val += I.at<Vec3b>(r+1,c).val[k]*(dr)*(1-dc);
-    val += I.at<Vec3b>(r,c+1).val[k]*(1-dr)*(dc);
-    val += I.at<Vec3b>(r+1,c+1).val[k]*(dr)*dc;
-    int intensity = round(val);
-    return intensity;
+Mat rotate(Mat &I, double angle, Interpolation ip){
+
+  int channels = I.channels();
+  int nRows = I.rows;
+  int nCols = I.cols;
+
+  double radian = degree_to_radian(angle);
+  double sine = sin(radian);
+  double cosine = cos(radian);
+
+  if(angle>=180) angle-=360;
+
+
+  double diagonal = sqrt(nRows*nRows + nCols*nCols);
+  double theta = radian_to_degree(atan(nRows*1.0/nCols));
+
+  int newRow = 0;
+  int newCol = 0;
+  double theta_x = 0;
+  double theta_y = 0;
+
+  if(0 <= angle && angle < 90){
+    // First Quad
+    theta_x = degree_to_radian(angle + theta);
+    theta_y = degree_to_radian(angle - theta);
+    newRow = round(abs(diagonal*sin(theta_x)));
+    newCol = round(abs(diagonal*cos(theta_y)));
   }
+
+  else if(-90 <= angle && angle < 0){
+    // Fourth Quad
+    theta_x = degree_to_radian(angle - theta);
+    theta_y = degree_to_radian(angle + theta);
+
+    newRow = round(abs(diagonal*sin(theta_x)));
+    newCol = round(abs(diagonal*cos(theta_y)));
+  }
+
+  else if(90 <= angle && angle < 180){
+    // Second
+    theta_x = degree_to_radian(angle - theta - 90);
+    theta_y = degree_to_radian(angle + theta - 90);
+
+    newRow = round(abs(diagonal*cos(theta_x)));
+    newCol = round(abs(diagonal*sin(theta_y)));
+  }
+
+  else {
+    theta_x = degree_to_radian(angle + theta + 90);
+    theta_y = degree_to_radian(angle - theta + 90);
+
+    newRow = round(abs(diagonal*cos(theta_x)));
+    newCol = round(abs(diagonal*sin(theta_y)));
+  }
+
+  cout<<newRow<<" "<<newCol<<endl;
+
+  int in_origin_x = floor(0.5*nRows);
+  int in_origin_y = floor(0.5*nCols);
+
+  int out_orgin_x = floor(0.5*newRow);
+  int out_orgin_y = floor(0.5*newCol);
+
+  Mat output(newRow, newCol, CV_8UC3);
+
+  Vec3b *p,*q;
+
+  for(int i=0;i<newRow;i++){
+    
+    int shifted_i = i - out_orgin_x;
+
+    p = I.ptr<Vec3b>(i);
+    q = output.ptr<Vec3b>(i);
+
+    for(int j=0;j<newCol;j++){
+
+      int shifted_j = j - out_orgin_y;
+
+      double rotate_x = cosine*shifted_i - sine*shifted_j + in_origin_x;
+      double rotate_y = sine*shifted_i + cosine*shifted_j + in_origin_y;
+
+      for(int k=0;k<channels;k++){
+        if(ip == nearest){
+            int r = round(rotate_x);
+            int c = round(rotate_y);
+
+            if(inRange(r,0,nRows) && inRange(c, 0, nCols)) q[j][k] = nearestNeighbour(I, r, c ,k);
+        } 
+
+        else if(ip == bilinear){
+
+          int r = floor(rotate_x);
+          int c = floor(rotate_y);
+
+          double dr = rotate_x - r;
+          double dc = rotate_y - c;
+
+          if(inRange(r,0,nRows) && inRange(c, 0, nCols)) q[j][k] = bilinearInterpolation(I, r, c, dr, dc, k);
+        } 
+      }
+
+    }
+  }
+  return output;
+
+}
+  
 
 };
 
@@ -85,14 +233,14 @@ public:
 int main( int argc, char** argv ) {
   
   Mat image;
-  image = imread("la.png" , CV_LOAD_IMAGE_COLOR);
+  image = imread("abc.jpg" , 1);
   
   if(! image.data ) {
       cout <<  "Could not open or find the image" << endl ;
       return -1;
     }
   AffineTransformation a;
-  image = a.resize(image, 1.5);
+  image = a.rotate(image, 45, bilinear);
   namedWindow( "Display window", WINDOW_AUTOSIZE );
   imshow( "Display window", image );
   
